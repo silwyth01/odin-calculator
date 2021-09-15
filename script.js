ab3c4788c2004585ee4d0c7d48a9947c1d4e6a3d
Silwyth <silwyth01@gmail.com>
2021-09-14 23:05:07 -0400

    Ajout des fonctions operateurs, tentative d'intégrer les fonctions en réponses aux clicks

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const output = document.querySelector(".output");
 const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
+const clear = document.querySelector(".all-clear");
+
+let numero = [];
+let operatio = "";
+let total = 0;
+
+
+
+clear.addEventListener('click', () => {
+    output.innerText = null;
+})
 
 operations.forEach(operation => {
     operation.addEventListener('click', () => {
         output.innerText = operation.innerText;
+        if (output.innerText === '*') {
+            operatio = '*';
+        } else if (output.innerText === '+') {
+            operatio = '+';
+        }
+        else if (output.innerText === '÷') {
+            operatio = '/';
+        }
+        else operatio = '-';
     })
 })
 
@@ -13,3 +33,25 @@ numbers.forEach(number => {
         output.innerText = number.innerText;
     })
 })
+
+function adder(a, b) {
+    return a + b;
+}
+
+function multiplier(a, b) {
+    return a * b;
+}
+
+function divider(a, b) {
+    return a / b;
+}
+
+function substracter(a, b) {
+    return a - b;
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
+
